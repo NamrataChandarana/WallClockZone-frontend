37d4be9cce736f87d6a151c230d4677944e28746
Namrata Chandarana <namratachandarana20@gmail.com>
2024-06-21 13:38:47 +0530

    404 page

---
+++ src/pages/Pagenotfound.jsx
@@ -0,0 +1,20 @@
+import { Heading, Box } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Pagenotfound = () => {
+  return (
+    <Box className="pnf">
+       <Box className="text-3xl m-auto">
+              404 <br/>
+              Page not found!
+        </Box>
+
+      <Link to="/" className="pnf-btn">
+        Go Back
+      </Link>
+    </Box>
+  );
+};
+
+export default Pagenotfound;
